229de8ddc4ebaccb240c01ce5307415d872ed81f
Michal Bobrowski <michal.bobrowski@op.pl>
2020-08-21 08:20:22 +0200

    added photo description to apod

--- nasa_apod.py
+++ nasa_apod.py
@@ -20,12 +20,22 @@ def get_apod():
     response: Response = requests.get(URL_APOD, params=params)
 
     image_path = json.loads(response.text)['url']
+    image_description = json.loads(response.text)['explanation']
 
     response = requests.get(image_path)
     image_bytes = io.BytesIO(response.content)
 
     image = Image.open(image_bytes)
     image.show()
+
+    print_photo_information(str(date), image_description)
+
+
+def print_photo_information(current_date: str, image_description: str):
+    print('Current date: ' + current_date + '\n\n')
+    print('Image description: ' + '\n')
+    print(image_description + '\n\n')
+    input('Press Enter to continue...')
 
 
 get_apod()
